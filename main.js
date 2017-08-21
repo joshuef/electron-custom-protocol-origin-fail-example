a434841ebebcbd5425d50663b6aaf3b704f1294d
Josh Wilson <joshuef@gmail.com>
2017-08-21 20:00:00 +0200

    Add custom protocol and example req to web server that responds with AccessControlAllowOrigin headers and causes crash

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const electron = require('electron')
+const protocol = electron.protocol;
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
@@ -7,6 +8,26 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
+
+protocol.registerStandardSchemes(['custom'], {secure:true});
+
+// Custom protocol that maps to localhost
+const registerCustomProtocol = () => {
+  protocol.registerHttpProtocol( 'custom', (req, cb) => {
+    const parsed = url.parse(req.url);
+
+    if (!parsed.host) { return; }
+
+    const path = parsed.pathname;
+    const port = parsed.port;
+    const newUrl = `http://localhost:${port}${path}`;
+
+    cb({ url: newUrl });
+  });
+};
+
+app.on('gpu-process-crashed', e=> console.log('crasssh', e))
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -14,16 +35,10 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  registerCustomProtocol();
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -32,7 +47,48 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  // essentially the same request works fine...
+  // mainWindow.loadURL( 'http://localhost:8888')
+
+
+  mainWindow.loadURL( 'custom://p:8888')
+
+  let filter = {
+      urls: ['http://*/*', 'https://*/*' ]
+
+    }
+
+
+    mainWindow.webContents.session.webRequest.onHeadersReceived(filter, (details, cb ) =>
+    {
+      let responseHeaders = Object.assign( {}, details.responseHeaders  )
+
+         console.log("Headers received:", responseHeaders);
+
+		// alternatively delete AccessControAllowOrigin to prevent crash
+		//   delete responseHeaders['Access-Control-Allow-Origin'];
+      cb( { responseHeaders } )
+    })
+
+
+    mainWindow.webContents.session.webRequest.onBeforeSendHeaders(filter, (details, cb) =>
+	{
+		let requestHeaders = Object.assign( {}, details.requestHeaders  )
+
+		console.log("Headers sent: ", requestHeaders);
+
+		// uncomment and webview/window does not crash.
+		// delete requestHeaders.Origin;
+
+		cb({ requestHeaders })
+	})
+
+
 }
+
+
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
